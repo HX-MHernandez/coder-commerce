3a35991c46b84d0a7f09451407f7096b85481752
mate444 <mateoeo23@gmail.com>
2023-01-11 22:47:55 -0300

    models - User

---
+++ models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace coder_commerce.models
+{
+    public class User
+    {
+        public int id;
+        public string name;
+        public string surname;
+        public string username;
+        public string password;
+        public string email;
+    }
+}
