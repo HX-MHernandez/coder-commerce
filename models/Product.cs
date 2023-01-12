879e0c7b28c3875064a74556786b1a9c65603ed2
mate444 <mateoeo23@gmail.com>
2023-01-11 22:48:24 -0300

    models - Adds Product model

---
+++ models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace coder_commerce.models
+{
+    public class Product
+    {
+        public int id;
+        public string description;
+        public double price;
+        public double cost;
+        public int stock;
+        public int userId;
+        public int categoryId;
+    }
+}
